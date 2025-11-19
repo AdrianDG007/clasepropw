3b79fcee6fcb0b7dab60656b416a69a28e4e8155
AdrianDG007 <adriandgrp@gmail.com>
2025-11-19 21:47:18 +0100

    Jorge Manrrique
    Cierra la clase de hoy

--- Programacio-4/src/Main.java
+++ Programacio-4/src/Main.java
@@ -1,9 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando programa de operadores");
-        Operadores operadores = new Operadores();
-        operadores.operadoresAritmeticos();
+        Operadores operadoresVariables = new Operadores();
+        //operacionesVariable.OperaciosAritmeticas();
+        operadoresVariables.operadoresAritmeticos();
+        System.out.println();
+        operadoresVariables.operadoresAsignacion();
+        System.out.println();
         Calculadora miCalculadora = new Calculadora();
-
+        operadoresVariables.operadoresComparacion();
+        System.out.println();
+        //BufferedReader
+        operadoresVariables.operadoresLogicos();
+        Scanner lectroTeclado = new Scanner(System.in);
+        System.out.println("Dime tu nomrbe y apellido");
+        String nombre = lectroTeclado.nextLine();
+        System.out.println("Que salario quieres ganar");
+        double salarioEntrada =lectroTeclado.nextDouble();
+        System.out.println("Que edad tienes");
+        int edad = lectroTeclado.nextInt();
+        System.out.println("Tienes permiso de conducir");
+        boolean conducir = lectroTeclado.nextBoolean();
+        operadoresVariables.evalaurCandidato(salarioEntrada,edad,conducir, nombre);
     }
 }
--- Programacio-4/src/Operadores.java
+++ Programacio-4/src/Operadores.java
@@ -37,12 +37,55 @@ public class Operadores {
 
     public void operadoresAsignacion(){
         int operadorUno = 10;
+        int operadorDos =6;
         //operadorUno = operadorUno +5;
         operadorUno +=5;// en operadorUno guarda la suma de lo que valia operdaorUno + 5
-        operadorUno +=15;
+        //operadorUno +=15;
+        operadorUno +=operadorDos;
         System.out.println("El valor de operador 1 es "+operadorUno);
+        operadorUno-=5; // 16 operadorUno = operadorUno -5
+        operadorUno*=2; // 32 operadorUno = operadorUno *2
+        operadorUno%=2; // 0  operadorUno = operadorUno %2
+        System.out.println("El resultado de las asignaciones completas es de "+operadorUno);
 
 
+   }
+
+   //seran utilizados cmo condicionespara ejecutar una parte del codigo u otra
+   public void operadoresComparacion(){
+       int operandoUno = 10;
+       int operandoDos = 20;
+       boolean resultado= operandoUno>operandoDos;
+       System.out.println("El resultado de la comparacion es "+resultado);
+       resultado = operandoUno<operandoDos;
+       System.out.println("El resultado de la comparcion "+resultado);
+       resultado = operandoUno<=operandoDos;
+       resultado = operandoUno>=operandoDos;
+       resultado = operandoUno==operandoDos;
+       resultado = operandoUno!=operandoDos;
+       //cuanto quires cobrar
+       //cuantos años tienes
+       //tienes carnet
+   }
+
+   public void  operadoresLogicos(){
+        int sueldo = 20000;
+        int edad = 35;
+        boolean conducir = true;
+        //conducir = !conducir;
+       System.out.println("El candidato peude conducir: "+!conducir);
+        //cuando quiere cobrar menos de 30000 y tienes menos de 40 y puede conducir
+        boolean candidadtoValido = sueldo<30000 && edad<40 && conducir; //true
+       candidadtoValido = (sueldo<20000) || (edad<25 || conducir && sueldo>25000 && edad> 60);
+                        // F           || T || T||
+                        //T && F && F -> F
+       System.out.println("El candidato es valido: "   +candidadtoValido);
+   }
+
+   public void evalaurCandidato(double sueldo, int edad, boolean conducir, String nombre){
+        boolean resultado= sueldo<=20000 && edad<40 &&conducir;
+       System.out.println("Evaluando al candidato "+nombre);
+       System.out.println("La evaluacion del canididato es: "+resultado);
    }
 }
 
