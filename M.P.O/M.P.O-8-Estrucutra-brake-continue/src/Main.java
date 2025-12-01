e1ab3d66e4068f39604eeb654b8e5af387467bb3
AdrianDG007 <adriandgrp@gmail.com>
2025-12-01 21:52:42 +0100

    a
    a

--- M.P.O/M.P.O-8-Estrucutra-brake-continue/src/Main.java
+++ M.P.O/M.P.O-8-Estrucutra-brake-continue/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     //break ->para la repeticion en el momento donde se indica. SIEMPRE es la ultima linea del cuerpo de repeticon
+    //return -> retorna un valor (el metodo). el metodo termina !!! el reutrn es la ultima linea de metodo
     public static void main(String[] args) {
         int acumulador = 0;
         //repetitiva
@@ -25,16 +26,36 @@ public class Main {
             System.out.println("El valor ya lo has alcanzado44");
             //break;
         }*/
-        for (int i = 0; i < 10; i++) {
+        /*for (int i = 0; i < 10; i++) {
             System.out.println( "La tabla del "+ i);
             for (int j = 0; j < 11; j++) {
                 System.out.printf("\t El resultado de %d * %d = %d%n", i, j, i*j);
-                if (j == 5){
+                if (j == 1){
                     break;
                 }
             }
 
         }
+
+         */
+        /*do {
+            acumulador *= (int) (Math.random() * 41);
+            return;
+        }while (acumulador <= 301);
+         */
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println("Imprimmiendo datos en posicion");
+            return;
+        }
+
+         */
+        /*do {
+            acumulador += (int)  (Math.random() * 41);
+            return;
+        }
+        while (acumulador<301);
+
+         */
         System.out.println("El acumulador tiene valor de " + acumulador);
         System.out.println("Termianando el programa ");
     }
