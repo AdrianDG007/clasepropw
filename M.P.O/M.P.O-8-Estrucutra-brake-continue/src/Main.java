64a9d3b439799e97a5c767fb8135e86fd1ce70c9
AdrianDG007 <adriandgrp@gmail.com>
2025-12-02 12:23:34 +0100

    a
    a

--- M.P.O/M.P.O-8-Estrucutra-brake-continue/src/Main.java
+++ M.P.O/M.P.O-8-Estrucutra-brake-continue/src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     //break ->para la repeticion en el momento donde se indica. SIEMPRE es la ultima linea del cuerpo de repeticon
-    //return -> retorna un valor (el metodo). el metodo termina !!! el reutrn es la ultima linea de metodo
+    //return -> retorna un valor (el metodo). el metodo termina!!!!el reutrn es la ultima linea de metodo
+    // continue -> vs a "pasar" el turno cunado te indique
+    // if -> evalua una condicion y decide si entrea en un cuerpo o en otro
+    // if ternario .> decide el VALOR de una variable depedeindo de una condicion logica
     public static void main(String[] args) {
-        int acumulador = 0;
+        int acumulador = 1000;
         //repetitiva
         /*
         for (int i = 0; i < 10; i++) {
@@ -56,7 +59,29 @@ public class Main {
         while (acumulador<301);
 
          */
-        System.out.println("El acumulador tiene valor de " + acumulador);
+        /*System.out.println("El acumulador tiene valor de " + acumulador);
         System.out.println("Termianando el programa ");
+        int [] numeros = {1, 4, 34, 56, -12, 56, 78, 12, -56};
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < 0) {
+                continue;
+            }
+            System.out.println(numeros[i]);
+        }
+
+         */
+        /*if (nota > 5){
+            System.out.println("Examen aprobado");
+        } else {
+            System.out.println("Examen suspenso");
+        }
+
+         */
+        //variable = condicion logica ? " val de a variable cuiando es true" : "val de la variable cuando es false"
+        int nota = 5;
+        String resultado = nota < 5 ? "suspesno" : "aprobado";
+        char letraNota = nota < 5 ? 'A' : 'P';
+        
+
     }
 }
