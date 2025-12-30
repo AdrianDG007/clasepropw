8bbde4ae489de506ffe4522419c81bc62f784816
AdrianDG007 <adriandgrp@gmail.com>
2025-12-30 15:05:54 +0100

    a
    a

---
+++ Programacion/Programacion-Objetos-12/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        int numero = 5;
+        Integer numeroComplejo = 5;
+        Scanner scanner = new Scanner(System.in);
+    }
+}
